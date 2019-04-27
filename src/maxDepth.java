7ab8c64db08faf84bc5b9c81012e49fd256a1d59
uzig305 <zjt_james@163.com>
2019-04-27 21:56:47 +0800

    树的最大深度

---
+++ src/maxDepth.java
@@ -0,0 +1,18 @@
+import base.TreeNode;
+
+/**
+ * created by Zheng Jiateng on 2019/4/27.
+ */
+
+public class maxDepth {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        if(root.left == null && root.right == null){
+            return 1;
+        } else{
+            return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        }
+    }
+}
