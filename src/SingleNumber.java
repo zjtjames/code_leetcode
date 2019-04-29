6e643b62c583c996c4b50284ec1ecd21a6d36cae
uzig305 <zjt_james@163.com>
2019-04-29 10:47:42 +0800

    只出现了一次的数字

--- src/SingleNumber.java
+++ src/SingleNumber.java
@@ -4,10 +4,13 @@
 
 /**
  * 找出数组中唯一一个只出现了一次的数字 136. Single Number
- * 位运算 异或
  */
 public class SingleNumber {
-    public static void main(String[] args) {
-        System.out.println(4^1^4^1^2);
+    public int singleNumber(int[] nums) {
+        int result = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            result ^= nums[i];
+        }
+        return result;
     }
 }
