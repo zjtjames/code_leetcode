3f189defea8883f419ac96c1c9b80059676ddf91
uzig305 <zjt_james@163.com>
2019-04-29 16:38:47 +0800

    删除列表结点

---
+++ src/DeleteNode.java
@@ -0,0 +1,15 @@
+/**
+ * created by Zheng Jiateng on 2019/4/29.
+ */
+
+import base.ListNode;
+
+/**
+ * 删除列表结点 237. Delete Node in a Linked List
+ */
+public class DeleteNode {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
