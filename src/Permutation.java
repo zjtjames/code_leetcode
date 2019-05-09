d37f912b9d2cab47f7b715c3f6e0edebd00fd1d7
uzig305 <zjt_james@163.com>
2019-05-09 15:01:17 +0800

    回溯法典型题 permutation

---
+++ src/Permutation.java
@@ -0,0 +1,39 @@
+/**
+ * created by Zheng Jiateng on 2019/5/9.
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 46. Permutations 排列
+ * Given a collection of distinct integers, return all possible permutations.
+ * Input: [1,2,3]
+ * Output: [[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], [3,2,1]]
+ *
+ * 回溯法(backtracking)的典型题
+ */
+public class Permutation {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums); // 不必要
+        backtrack(result, new ArrayList<>(), nums);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] nums) {
+        if (tempList.size() == nums.length) {
+            result.add(new ArrayList<>(tempList));
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (tempList.contains(nums[i])) {
+                    continue; // 元素已经包含 跳过
+                }
+                tempList.add(nums[i]);
+                backtrack(result, tempList, nums);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}
