37b72ee367ac6f5a601ccb68bcc7b2b1d2f08c73
uzig305 <zjt_james@163.com>
2019-05-09 15:49:30 +0800

    回溯法典型题 permutation doc

--- src/Permutation.java
+++ src/Permutation.java
@@ -15,25 +15,30 @@ import java.util.List;
  * 回溯法(backtracking)的典型题
  */
 public class Permutation {
-    public List<List<Integer>> permute(int[] nums) {
+    public static List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums); // 不必要
         backtrack(result, new ArrayList<>(), nums);
         return result;
     }
 
-    private void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] nums) {
+    private static void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] nums) {
         if (tempList.size() == nums.length) {
-            result.add(new ArrayList<>(tempList));
+            result.add(new ArrayList<>(tempList)); // 深复制
         } else {
             for (int i = 0; i < nums.length; i++) {
                 if (tempList.contains(nums[i])) {
                     continue; // 元素已经包含 跳过
                 }
                 tempList.add(nums[i]);
-                backtrack(result, tempList, nums);
-                tempList.remove(tempList.size() - 1);
+                backtrack(result, tempList, nums); // 因为for循环的存在 每次回溯可以把下一位的可能的结果都包括上
+                tempList.remove(tempList.size() - 1); // 这一位的这个数字所有情况罗列完后 删除这一位的这个数字 进入for循环的下一个数字
             }
         }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        permute(nums);
     }
 }
