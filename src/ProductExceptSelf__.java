5a05fc10acf4dcd7eda9009685b65df64cdac23b
uzig305 <zjt_james@163.com>
2019-05-10 11:52:13 +0800

    238. Product of Array Except Self

---
+++ src/ProductExceptSelf__.java
@@ -0,0 +1,24 @@
+/**
+ * created by Zheng Jiateng on 2019/5/10.
+ */
+
+/**
+ * 除了自己之外的数字的乘积 238. Product of Array Except Self
+ * 要求 o(n)的时间复杂度和0(1)的空间复杂度 The output array does not count as extra space for the purpose of space complexity analysis.
+ */
+public class ProductExceptSelf__ {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] output = new int[n];
+        output[0] = 1;
+        for(int i = 1; i < n; i++){
+            output[i] = output[i-1] * nums[i-1]; // 此时output[i]表示i左边的乘积
+        }
+        int right = 1;
+        for(int i = n-1; i >= 0; i--){
+            output[i] *= right;
+            right *= nums[i];
+        }
+        return output;
+    }
+}
