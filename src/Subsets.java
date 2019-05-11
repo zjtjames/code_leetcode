e2e07394b2d082ad97832012ff57fb2e58430526
uzig305 <zjt_james@163.com>
2019-05-11 23:37:37 +0800

    子集 78.Subsets

---
+++ src/Subsets.java
@@ -0,0 +1,31 @@
+/**
+ * created by Zheng Jiateng on 2019/5/11.
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 子集 78. Subsets
+ *
+ * 回溯法(backtracking)的典型题
+ */
+public class Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(result, new ArrayList<>(), nums, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] nums, int start){
+        result.add(new ArrayList(tempList));
+        for(int i = start; i < nums.length; i++){
+            tempList.add(nums[i]);
+            backtrack(result, tempList, nums, i + 1);
+            tempList.remove(tempList.size() - 1);
+        }
+
+    }
+}
