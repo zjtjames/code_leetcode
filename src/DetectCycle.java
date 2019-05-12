0d2d26ec8f7f6bb481c31c88a77810c84f7ccf1c
uzig305 <zjt_james@163.com>
2019-05-12 23:02:00 +0800

    弗洛伊德判圈法

---
+++ src/DetectCycle.java
@@ -0,0 +1,35 @@
+/**
+ * created by Zheng Jiateng on 2019/5/12.
+ */
+
+import base.ListNode;
+
+/**
+ * 142. Linked List Cycle II  找链表的圈的起点
+ *
+ * 弗洛伊德判圈法
+ */
+public class DetectCycle {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode quick = head;
+        boolean isCycle = false;
+        while(quick != null && quick.next != null){
+            quick = quick.next.next;
+            slow = slow.next;
+            if(quick == slow){
+                isCycle = true;
+                break;
+            }
+        }
+        if(!isCycle){
+            return null;
+        }
+        quick = head;
+        while(quick != slow){
+            quick = quick.next;
+            slow = slow.next;
+        }
+        return quick;
+    }
+}
