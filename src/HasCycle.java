5718fab4db69261e95b3bfa841bfacd9a46fe749
uzig305 <zjt_james@163.com>
2019-05-14 11:32:18 +0800

    141. Linked List Cycle

---
+++ src/HasCycle.java
@@ -0,0 +1,25 @@
+/**
+ * created by Zheng Jiateng on 2019/5/14.
+ */
+
+import base.ListNode;
+
+/**
+ * 141. Linked List Cycle
+ *
+ * 弗洛伊德判圈法
+ */
+public class HasCycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode quick = head;
+        ListNode slow = head;
+        while(quick != null && quick.next != null){
+            quick = quick.next.next;
+            slow = slow.next;
+            if(quick == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
