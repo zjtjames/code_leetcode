fbdde199bccceee05752de1775b1ff7eb0acea5e
uzig305 <zjt_james@163.com>
2019-05-29 23:43:06 +0800

    判断二叉树是不是对称的

---
+++ src/IsSymmetric.java
@@ -0,0 +1,23 @@
+/**
+ * created by Zheng Jiateng on 2019/5/29.
+ */
+
+import base.TreeNode;
+
+/**
+ * 101. Symmetric Tree
+ *
+ * 判断二叉树是不是对称 递归法和迭代法
+ */
+public class IsSymmetric {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        return core(root.left, root.right);
+    }
+
+    private boolean core(TreeNode left, TreeNode right){
+        if(left == null && right == null) return true;
+        if(left == null || right == null || left.val != right.val) return false;
+        return core(left.left, right.right) && core(left.right, right.left);
+    }
+}
