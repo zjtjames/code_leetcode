73a2172bfa281bcb04cb9c757dc618bccc1ea8a3
uzig305 <zjt_james@163.com>
2019-06-11 00:08:26 +0800

    56. Merge Intervals

--- src/MergeInterval.java
+++ src/MergeInterval.java
@@ -6,8 +6,15 @@ import java.util.Comparator;
  * created by Zheng Jiateng on 2019/6/10.
  */
 
+/**
+ * 56. Merge Intervals
+ * 先用每个interval的起始点对二维数组进行排序
+ * 然后遍历 当新来的interval的起点大于merged中最后一个interval的终点时，二者不重叠，merged加入新interval
+ * 否则 二者重叠 merged中最后一个interval的终点更新为二者终点之间较大者
+ */
 public class MergeInterval {
     public int[][] merge(int[][] intervals) {
+        if (intervals.length < 1) return intervals;
         Arrays.sort(intervals, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
@@ -19,10 +26,10 @@ public class MergeInterval {
         for (int[] interval : intervals) {
             if (cur == 0 || interval[0] > merged[cur - 1][1]) {
                 merged[cur++] = interval;
-            } else {
-
+            } else if (merged[cur - 1][1] < interval[1]){
+                merged[cur - 1][1] = interval[1];
             }
         }
-
+        return Arrays.copyOfRange(merged, 0, cur);
     }
 }
