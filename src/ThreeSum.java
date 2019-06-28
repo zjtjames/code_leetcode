08d7a5e844042db969bcf30d5cdf9fdc1868ddbd
James Zheng <zjtjames@gmail.com>
2019-06-28 12:01:29 +0800

    three sum

---
+++ src/ThreeSum.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Zheng Jiateng on 2019/6/28.
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 15. 3Sum 和为0的三个数字的所有情况
+ *
+ * Given array nums = [-1, 0, 1, 2, -1, -4], The solution set must not contain duplicate triplets.
+ * A solution set is:
+ * [[-1, 0, 1],[-1, -1, 2]]
+ */
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.asList()
+    }
+}
