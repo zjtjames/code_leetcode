837e25a25e91cf67e8974ebfe74652d2c191b688
uzig305 <zjt_james@163.com>
2019-07-03 00:24:13 +0800

    避免重复答案

--- src/ThreeSum__.java
+++ src/ThreeSum__.java
@@ -16,29 +16,27 @@ import java.util.List;
  * 思路：先对数组排序 然后每选定一个数字 对右边数组做两头寻找版的two sum
  * 时间复杂度o(n2)
  */
-public class ThreeSum {
+public class ThreeSum__ {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         int len = nums.length;
         List<List<Integer>> result = new ArrayList<>();
         if (len < 3) return result;
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < len - 2; i++) { // 最多以倒数第三个数字开头
             if (nums[i] > 0) break;
-            if (i > 0 && nums[i] == nums[i -1]) continue;
+            if (i > 0 && nums[i] == nums[i -1]) continue; // 避免重复答案
             int target = 0 - nums[i];
             int lo = i + 1;
             int hi = len - 1;
             while (lo < hi) {
                 if (nums[lo] + nums[hi] == target) {
-                    List<Integer> oneAnswer = new ArrayList<>();
-                    oneAnswer.add(nums[i]);
-                    oneAnswer.add(nums[lo]);
-                    oneAnswer.add(nums[hi]);
-                    result.add(new ArrayList<>(oneAnswer));
-                    break;
-                } else if (nums[lo] + nums[hi] > target) {
+                    result.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                    while (lo < hi && nums[lo] == nums[lo +1]) lo ++; // 避免重复答案
+                    while (lo < hi && nums[hi] == nums[hi - 1]) hi--; // 避免重复答案
+                    lo++;
+                } else if (nums[lo] + nums[hi] > target)
                     hi--;
-                } else lo++;
+                else lo++;
             }
         }
         return result;
