a820866c5e7b3f4d2f6cf53f0ce6f3d24f8d722d
uzig305 <zjt_james@163.com>
2019-07-10 20:56:08 +0800

    更准确的思路

--- src/SortColors__.java
+++ src/SortColors__.java
@@ -14,16 +14,14 @@ public class SortColors__ {
 
     public void sortColors(int[] nums) {
         int slow = 0;
-        int k = nums.length - 1;
-        // 交换时，快指针指向0 慢指针指向1，交换的目的是把快指针遇到的优先级高的元素换到前面来,没遇到优先级高的元素就不换。
-        // 慢指针在后面 快指针先遇到2的时候就把2甩到后面去了 所以慢指针只能指向1
-        for(int i = 0; i <= k; i++){
-            // 遇到优先级高的元素，快、慢指针都走；遇到优先级低的元素，只有快指针能走，所以慢指针自然指向优先级低的元素。
-            if(nums[i] == 0){
+        int last = nums.length - 1;
+        for(int i = 0; i <= last; i++){
+            // 把2甩到最后，减小数组长度，i回退1，使得可以再次检查从队尾换来的元素
+            if(nums[i] == 2){
+                exchange(nums, i--, last--);
+                // 与MoveZeroers完全一样 把1换到后面去 遇到0就交换
+            } else if(nums[i] == 0){
                 exchange(nums, i, slow++);
-                // 把2甩到最后，减小数组长度，i回退1，使得可以再次检查从队尾换来的元素
-            } else if(nums[i] == 2){
-                exchange(nums, i--, k--);
             }
         }
     }
