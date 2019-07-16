54855fa5f45abace45a8a62e8a91271aa30b2bc9
uzig305 <zjt_james@163.com>
2019-07-16 22:30:48 +0800

    dp

--- src/UniquePaths__.java
+++ src/UniquePaths__.java
@@ -11,24 +11,24 @@ public class UniquePaths__ {
         if(m<1 || n<1){
             return 0;
         }
-        int[][] products = new int[n][m];
+        int[][] dp = new int[n][m];
         // 要到products[n-1][m-1]
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
                 if(i == 0 && j == 0){
-                    products[i][j] = 0;
+                    dp[i][j] = 0;
                 }
                 else if(i == 0 && j > 0){
-                    products[i][j]  = 1;
+                    dp[i][j]  = 1;
                 }
                 else if(i > 0 && j == 0){
-                    products[i][j] = 1;
+                    dp[i][j] = 1;
                 } else{
-                    products[i][j] = products[i][j-1] + products[i-1][j];
+                    dp[i][j] = dp[i][j-1] + dp[i-1][j];
                 }
             }
         }
-        products[0][0] = 1;
-        return products[n-1][m-1];
+        dp[0][0] = 1;
+        return dp[n-1][m-1];
     }
 }
