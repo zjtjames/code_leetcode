c84da3a8b7097126b1a6c89e31007fb0e5a6e1e0
uzig305 <zjt_james@163.com>
2019-07-21 00:44:45 +0800

    判断子树

---
+++ src/IsSubTree__.java
@@ -0,0 +1,43 @@
+/**
+ * created by Zheng Jiateng on 2019/7/20.
+ */
+
+import base.TreeNode;
+
+/**
+ * 572. Subtree of Another Tree
+ * * 一棵树是不是另一棵树的子树
+ * s 的一个子树应包括 s 的一个节点和这个节点的所有子孙。
+ * eg:
+ * 给定的树 s：
+     3
+    / \
+   4   5
+  / \
+ 1   2
+    /
+   0
+ 给定的树 t：
+   4
+  / \
+ 1   2
+
+ 返回 false。
+ */
+public class IsSubTree__ {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        return traverse(s, t);
+    }
+
+    // 判断两棵树是否完全相等
+    private boolean equals(TreeNode x, TreeNode y) {
+        if(x == null && y == null) return true;
+        else if(x == null || y == null) return false;
+        else return x.val == y.val && equals(x.left, y.left) && equals(x.right, y.right);
+    }
+
+    // 遍历s树 直到找到子树或s树为null了
+    private boolean traverse(TreeNode s, TreeNode t) {
+        return s != null && (equals(s, t) || traverse(s.left, t) || traverse(s.right, t));
+    }
+}
