2b6b359747f3c9851fc13a1ccc612c40c52f076c
uzig305 <zjt_james@163.com>
2019-08-05 23:58:21 +0800

    295. 数据流的中位数

---
+++ src/hard/MedianFinder.java
@@ -0,0 +1,23 @@
+/**
+ * created by Zheng Jiateng on 2019/8/5.
+ */
+package hard;
+
+/**
+ * 295. 数据流的中位数
+ *
+ */
+public class MedianFinder {
+    /** initialize your data structure here. */
+    public MedianFinder() {
+
+    }
+
+    public void addNum(int num) {
+
+    }
+
+    public double findMedian() {
+
+    }
+}
