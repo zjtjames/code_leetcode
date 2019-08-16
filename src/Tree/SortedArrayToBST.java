daab965f6c6c1843b3492e5b11b6208e2e896ad0
uzig305 <zjt_james@163.com>
2019-08-16 15:13:38 +0800

    refactor

--- src/Tree/SortedArrayToBST.java
+++ src/Tree/SortedArrayToBST.java
@@ -10,14 +10,22 @@ import java.util.Arrays;
 /**
  * 108. 将有序数组转换为二叉搜索树
  * 将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+ *
+ * 思路：不用每次复制数组 移动lo hi两个指针即可
  */
 public class SortedArrayToBST {
     public TreeNode sortedArrayToBST(int[] nums) {
-        int len = nums.length;
-        if(len < 1) return null;
-        TreeNode root = new TreeNode(nums[len / 2]);
-        root.left = sortedArrayToBST(Arrays.copyOfRange(nums, 0, len / 2));
-        root.right = sortedArrayToBST(Arrays.copyOfRange(nums, len / 2 + 1, len));
-        return root;
+        return core(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode core(int[] nums, int lo, int hi){
+        if(lo <= hi ){
+            int mid = (lo + hi) / 2;
+            TreeNode root = new TreeNode(nums[mid]);
+            root.left = core(nums, lo, mid - 1);
+            root.right = core(nums, mid + 1, hi);
+            return root;
+        }
+        return null;
     }
 }
