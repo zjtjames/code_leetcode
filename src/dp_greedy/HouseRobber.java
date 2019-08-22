f98561504d7233ab9eb70b8b9cbdaf1da7352454
uzig305 <zjt_james@163.com>
2019-08-22 16:43:51 +0800

    类似于背包问题

--- src/dp_greedy/HouseRobber.java
+++ src/dp_greedy/HouseRobber.java
@@ -6,22 +6,18 @@ package dp_greedy; /**
  * 198. House Robber
  *
  * 思路：dp典型题
+ * dp[i]为考虑到i为止的最高金额 有两种情况 抢i和不抢i 与背包问题很类似
+ * 让dp的索引比nums大1，设dp[0]=0，避免对边界条件的考虑
  */
 public class HouseRobber {
     public int rob(int[] nums) {
-        int len = nums.length;
-        if(len == 0) return 0;
-        if(len == 1) return nums[0];
-        if(len == 2) return Math.max(nums[0], nums[1]);
-        // dp[i]为包含i的到i为止的最大收入 dp[i]不是result[i]
-        int[] dp = new int[len];
-        dp[0] = nums[0];
-        dp[1] = nums[1];
-        dp[2] = nums[0] + nums[2];
-        for(int i = 3; i < len; i++){
-            dp[i] = Math.max(dp[i - 2], dp[i - 3]) + nums[i];
+        if(nums.length == 0) return 0;
+        int[] dp = new int[nums.length + 1]; // dp[i]为考虑到i为止的最高金额 有两种情况 抢i和不抢i
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            dp[i + 1] = Math.max(dp[i], dp[i - 1] + nums[i]);
         }
-        // 最大收入为dp[len - 1]与dp[len - 2]中较大的一个
-        return Math.max(dp[len - 1], dp[len - 2]);
+        return dp[nums.length];
     }
 }
